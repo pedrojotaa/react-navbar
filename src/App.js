fc706ab7824bc6f3186fe99336855a25b8e3523b
“João <eng.joao.pedro@outlook.com>
2023-01-05 14:30:44 -0300

    refactor: useRef to change style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import "./index.css";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [showLinks, setShowLinks] = useState(false);
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    /* is the height of the links */
+    const linkHeight = linksRef.current.height
+    console.log(linkHeight)
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linkHeight}`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks]);
 
   return (
     <nav>
@@ -19,13 +33,9 @@ function App() {
             <FaBars />
           </button>
         </div>
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
+        <div className="links-container" ref>
           {/* let's use react router */}
-          <ul className="links">
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
